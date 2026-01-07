802b65ada97b9f53c24859eabfbbf5db534fcfe4
kran-ji <jkran2252@gmail.com>
2026-01-07 19:56:37 +0800

    Updated LoginHandler to accept POST request

--- main.go
+++ main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
 	_ "github.com/lib/pq"
 )
+
+var db *sql.DB // Declare database in global
+
+type Credentials struct {
+	Username string `json:"username"`
+}
 
 const (
 	host     = "localhost"
@@ -21,7 +28,9 @@ func main() {
 	// Connect to database
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
+
+	var err error
+	db, err = sql.Open("postgres", psqlInfo)
 	err = db.Ping()
 	if err != nil {
 		log.Fatal(err)
@@ -34,14 +43,21 @@ func main() {
 }
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
-	// Front end Formatting
-	fmt.Fprintln(w, "Welcome to the Login Page!")
-
 	// User Request handling
 	requestType := r.Method
-	if requestType == http.MethodPost {
-		fmt.Println("Processing Login")
-	} else {
+	if requestType != http.MethodPost {
 		http.Error(w, "Method not allowed", 405)
+		return
 	}
+
+	var creds Credentials
+	err := json.NewDecoder(r.Body).Decode(&creds)
+	if err != nil {
+		http.Error(w, "Bad Request", 400)
+		return
+	}
+
+	// Front end Formatting
+	fmt.Fprintln(w, "Welcome to the Login Page!")
+	fmt.Println(creds.Username)
 }
